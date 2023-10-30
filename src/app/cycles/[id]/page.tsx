099f3a19f85ca57662d9d710f5526fcf0a6d14d0
Ángel Díaz <diazangel9311@gmail.com>
2023-10-29 21:12:06 -0500

    Add cycles/:id route

---
+++ src/app/cycles/[id]/page.tsx
@@ -0,0 +1,22 @@
+import CycleInfo from '@/components/cycle';
+import prisma from '@/db';
+import { notFound } from 'next/navigation';
+import React from 'react';
+
+async function getCycleById(id: number) {
+  return await prisma.cycle.findUnique({
+    where: { id },
+    include: { Expenses: true, Incomes: true },
+  });
+}
+
+async function Cycle({ params }: { params: { id: string } }) {
+  const cycle = await getCycleById(Number(params.id));
+  if (cycle) {
+    return <CycleInfo cycle={cycle} />;
+  }
+
+  notFound();
+}
+
+export default Cycle;
--- src/components/transaction/TransactionsSummary.tsx
+++ src/components/transaction/TransactionsSummary.tsx
@@ -1,4 +1,4 @@
-import { Decimal } from "@prisma/client/runtime/library";
+import { Decimal } from '@prisma/client/runtime/library';
 
 function TransactionsSummary({
   cycleBalance,
