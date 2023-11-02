74503730d082380ce4f64932a5a6d3e4d246399f
Ángel Díaz <diazangel9311@gmail.com>
2023-11-01 22:06:49 -0500

    remove CycleDetails from root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 import Link from 'next/link';
 import { getCurrentUser, getUserLastCycle } from '@/db';
-import CycleDetails from '@/components/cycle';
 
 async function HomePage() {
   const currentUser = await getCurrentUser();
-  const lastCycle = await getUserLastCycle(currentUser?.id);
 
   return (
     <div>
       <h1>Home</h1>
       <Link href="/profile">Profile</Link>
-      {lastCycle && <CycleDetails cycle={lastCycle} />}
     </div>
   );
 }
